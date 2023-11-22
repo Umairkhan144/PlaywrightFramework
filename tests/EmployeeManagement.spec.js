961a32122afe272dfd1524724d7dd3beb24943af
Umair Khan <umairkhan156@gmail.com>
2023-11-22 17:27:08 +0500

    Employee Management Test Cases Added

--- tests/EmployeeManagement.spec.js
+++ tests/EmployeeManagement.spec.js
@@ -1,9 +1,10 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../Pages/LoginPage.js');
 const ExcelUtils = require('../Utils/ExcelUtils.js');
+const loc = require('../Utils/locators.json')
 
 test('Verify user can redirect PIM screen', async ({page})=>{
     await page.goto('/')
-    await page.click("//span[normalize-space()='PIM']")
+    await page.click(loc.HomePage.PIMOption)
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList')
 })
